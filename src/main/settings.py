f30e1d1f152bca28f4e0c065048d0dc5b73d9b53
unknown <zehir.yasin@gmail.com>
2021-05-07 07:46:23 +0300

    feat: add model

--- src/main/settings.py
+++ src/main/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'pages.apps.PagesConfig'
+    'pages.apps.PagesConfig',
+    'courses.apps.CoursesConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC FILES
 
 
 STATIC_URL = '/static/'
